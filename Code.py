3df7ffaf72bcbffab46896c9d7f5acd8cd85343c
kavinsathishkumar <168625520+kavinsathishkumar@users.noreply.github.com>
2024-05-01 13:08:33 +0530

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+import warnings
+warnings.simplefilter("ignore")
+
+df = pd.read_excel("D:\Wind Prediction Project\Leap Green Five Year Data - Copy\output.xlsx")
+
+df.info()
